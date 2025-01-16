d1f2a58ce6fbddfe1628b9235cb9cf8b2eed7f48
mostafa hamed <Mostafa7ahmed>
2025-01-16 02:18:59 +0200

    Add call api2

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AUXAIL3N.mjs";import"./chunk-FIW6XJKL.mjs";import"./chunk-2OLLGEHW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-KIBK55IZ.mjs";import"./chunk-FIW6XJKL.mjs";import"./chunk-2OLLGEHW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
