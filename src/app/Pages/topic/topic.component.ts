cb36462531a9cfca70bcd8346c0f99a8d1d9dc01
mostafa hamed <Mostafa7ahmed>
2025-01-26 21:42:08 +0200

    Done

--- src/app/Pages/topic/topic.component.ts
+++ src/app/Pages/topic/topic.component.ts
@@ -19,13 +19,14 @@ export class TopicComponent {
     'Episode 5 ',
   ];
   timePeriods = [
-    'Bronze age',
-    'Iron age',
-    'Middle ages',
-    'Early modern period',
-    'Long nineteenth century',
-  
+    {"name": "New", "price": 81.00, "color": "#F5CD6D" , "border": "#F5CD6D40"},
+    {"name": "Preparation", "price": 81.00, "color": "#7371D5", "border": "#7371D540"},
+    {"name": "Shoofing", "price": 81.00, "color": "#E65656", "border": "#E6565640"},
+    {"name": "Recording", "price": 81.00, "color": "#C44CAA", "border": "#C44CAA40"},
+    {"name": "Published", "price": 81.00, "color": "#FF849D" , "border": "#FF849D40"}
+   
   ];
+  
 
   dropWitht(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
