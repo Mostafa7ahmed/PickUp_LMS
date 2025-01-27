a03e4627d0c24c2b9ad5d37ffac235eb8ba12f51
mostafa hamed <Mostafa7ahmed>
2025-01-27 04:11:05 +0200

    Sidebar daaaarrrrb

--- src/app/Pages/topic/topic.component.ts
+++ src/app/Pages/topic/topic.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
 
 @Component({
   selector: 'app-topic',
   standalone: true,
-  imports: [CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag , NzModalModule , NzButtonModule , NzIconModule , NzTabsModule],
+
   templateUrl: './topic.component.html',
   styleUrl: './topic.component.scss'
 })
@@ -26,7 +31,32 @@ export class TopicComponent {
     {"name": "Published", "price": 81.00, "color": "#FF849D" , "border": "#FF849D40"}
    
   ];
-  
+
+  tabs = [
+    {
+      name: 'Tab 1',
+      icon: 'apple'
+    },
+    {
+      name: 'Tab 2',
+      icon: 'android'
+    }
+  ];
+  isVisible = false;
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
 
   dropWitht(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
