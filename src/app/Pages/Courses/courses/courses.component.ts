30c5de86afc5e1d3a90f8b5ceb51ec60aa6f97c8
mostafa hamed <Mostafa7ahmed>
2025-03-04 06:12:43 +0200

    Add search and filter functionality to courses list; implement date range picker and dropdown toggle

--- src/app/Pages/Courses/courses/courses.component.ts
+++ src/app/Pages/Courses/courses/courses.component.ts
@@ -20,12 +20,18 @@ import { CardkanbanStageComponent } from '../Components/cardkanban-stage/cardkan
 import { KanbanService } from '../Core/service/kanban.service';
 import { ICourseKanban, IKanbanResponse, ITopicKanbaResult } from '../Core/interface/ikanban-response';
 import { MovecourseService } from '../Core/service/movecourse.service';
-import { AddCoursesComponent } from '../Components/add-courses/add-courses.component';
+import { FormsModule } from '@angular/forms';
+import { DatePicker } from 'primeng/datepicker';
+import { TopPopComponent } from "../../../Components/top-pop/top-pop.component";
+
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { AddCoursesComponent } from "../Components/add-courses/add-courses.component";
+
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule, ButtonModule,AddCoursesComponent, CardkanbanStageComponent, TabsModule, SplicTextPipe, MatTooltipModule, NgxEchartsModule, WidgetCoursesComponent, TableCoursesComponent],
+  imports: [CommonModule, NzSelectModule, CustomSelectComponent, ButtonModule, FormsModule, DatePicker, CardkanbanStageComponent, TabsModule, SplicTextPipe, MatTooltipModule, NgxEchartsModule, WidgetCoursesComponent, TableCoursesComponent, TopPopComponent, CustomSelectComponent, AddCoursesComponent],
   providers: [
     { provide: NGX_ECHARTS_CONFIG, useValue: { echarts } }
 
@@ -46,6 +52,7 @@ export class CoursesComponent implements OnInit {
 
 
   tableRecords: Record<string, any>[] = [];
+  rangeDates: Date[] = [];
 
 
   // End Call services
@@ -82,7 +89,11 @@ export class CoursesComponent implements OnInit {
     
   }
   //^ Functions
+  toggleDropdown(): void {
+    this.isOpen = !this.isOpen;
+    console.log(this.rangeDates[0].getDate())
 
+  }
   changeTab(value: number): void {
     this.valueheader = value;
   }
