07aa0e74f9a9bd6ba4ee9f86afa1da7d43db6ad1
mostafa hamed <Mostafa7ahmed>
2025-03-05 12:16:50 +0200

    Add custom select component with icon support; refactor courses component to integrate new select

---
+++ src/app/Pages/Courses/Components/customslectwithicon/customslectwithicon.component.ts
@@ -0,0 +1,40 @@
+import { FormControl } from '@angular/forms';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SplicTextPipe } from '../../Core/Pipes/splic-text.pipe';
+interface DropdownItem {
+  label: string;
+  color: string;
+}
+@Component({
+  selector: 'app-customslectwithicon',
+  standalone: true,
+  imports: [CommonModule, SplicTextPipe],
+  templateUrl: './customslectwithicon.component.html',
+  styleUrl: './customslectwithicon.component.scss'
+})
+export class CustomslectwithiconComponent {
+  @Input() items: any[] = [];
+  @Input() selectedItem: any | null = null;
+  @Output() selectionChange = new EventEmitter<any>();
+  isSelected: boolean = false;
+
+  isOpen = false;
+  isValidColor(color: string | undefined | null): boolean {
+    return /^#([0-9A-F]{3}){1,2}$/i.test(color || '');
+  }
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+    console.log(this.selectedItem)
+    console.log(this.selectionChange)
+    console.log(this.items)
+  }
+
+  selectOption(option: any) {
+    this.selectedItem = option;
+    this.selectionChange.emit(option);
+    this.isOpen = false;
+    this.isSelected = true;
+  }
+}
--- src/app/Pages/Courses/Core/service/paginate-courses.service.ts
+++ src/app/Pages/Courses/Core/service/paginate-courses.service.ts
@@ -15,16 +15,30 @@ export class PaginateCoursesService {
 
 
 
-   getCourses(topicId: number , pageNumber: number = 1, pageSize: number = 2, courseListViewType :number =0, orderBy: number = 2, orderDirection: number = 1 ,): Observable<any> {
-    const params = {
+   getCourses(
+    topicId: number, 
+    pageNumber: number = 1, 
+    pageSize: number = 2,
+
+    courseListViewType: number = 0, 
+    from?: string, 
+    to?: string, 
+    orderBy: number = 2, 
+    orderDirection: number = 1,
+
+  ): Observable<any> {
+  
+    const params: any = {
       topicId: topicId.toString(),
       courseListViewType: courseListViewType.toString(),
       orderBy: orderBy.toString(),
       pageNumber: pageNumber.toString(),
       pageSize: pageSize.toString(),
       orderBeforPagination: 'true',
-      orderDirection: orderDirection.toString()
+      orderDirection: orderDirection.toString(),
     };
+    if (from) params.from = from;
+    if (to) params.to = to;
 
     return this._HttpClient.get<any>(`${this.urlPagination}`, { params });
   }
