90931e0b19cb4c3e70be7ddb9638220e4f075b38
mostafa hamed <Mostafa7ahmed>
2025-03-05 15:51:01 +0200

    Refactor z-index values across components; enhance custom select functionality in Add Courses component

--- src/app/Pages/Courses/Components/add-courses/add-courses.component.ts
+++ src/app/Pages/Courses/Components/add-courses/add-courses.component.ts
@@ -1,17 +1,26 @@
 import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { TopPopComponent } from '../../../../Components/top-pop/top-pop.component';
 import { CustomSelectComponent } from '../../../../Components/custom-select/custom-select.component';
-import { NzSelectModule } from 'ng-zorro-antd/select';
 import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule, SlicePipe } from '@angular/common';
 import { TextHeaderComponent } from '../text-header/text-header.component';
 import { TooltipModule } from 'primeng/tooltip';
 import { SplicTextPipe } from '../../Core/Pipes/splic-text.pipe';
-
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+function alphabet(): string[] {
+  const children: string[] = [];
+  for (let i = 10; i < 36; i++) {
+    children.push(i.toString(36) + i);
+  }
+  return children;
+}
 @Component({
   selector: 'app-add-courses',
   standalone: true,
-  imports: [TopPopComponent, SplicTextPipe, TooltipModule, FormsModule, TextHeaderComponent, CommonModule, ReactiveFormsModule, NzSelectModule, CustomSelectComponent],
+  imports: [TopPopComponent, SplicTextPipe, TooltipModule, NzDividerModule, NzIconModule, NzInputModule, NzSelectModule, FormsModule, TextHeaderComponent, CommonModule, ReactiveFormsModule, NzSelectModule, CustomSelectComponent],
   templateUrl: './add-courses.component.html',
   styleUrl: './add-courses.component.scss'
 })
@@ -25,6 +34,19 @@ export class AddCoursesComponent {
   listOfTagOptions: string[] = [];
   showDescription: boolean = false;
   @Output() isAddPopupVisibleChange = new EventEmitter<boolean>();
+  listOfItem = ['jack', 'lucy'];
+  index = 0;
+  readonly listOfOption: Array<{ label: string; value: string }> = alphabet().map(item => ({
+    label: item,
+    value: item
+  }));
+
+  addItem(input: HTMLInputElement): void {
+    const value = input.value;
+    if (this.listOfItem.indexOf(value) === -1) {
+      this.listOfItem = [...this.listOfItem, input.value || `New item ${this.index++}`];
+    }
+  }
 
   private fb = new FormBuilder();
 
