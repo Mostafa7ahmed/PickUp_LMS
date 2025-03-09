8ed88987bcda06755691646cd14287f1883c464b
mostafa hamed <Mostafa7ahmed>
2025-03-09 18:59:41 +0200

    Enhance ViewTopicandStage component: update template to display dynamic topic details, improve styling, and integrate API service for fetching topic data

--- src/app/Pages/Topics/Components/view-topicand-stage/view-topicand-stage.component.ts
+++ src/app/Pages/Topics/Components/view-topicand-stage/view-topicand-stage.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import { TopicResult } from './../../Core/Interface/itopic';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Route, Router, RouterModule } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { TopPopComponent } from "../../../../Components/top-pop/top-pop.component";
 import { SpliceDescreptionPipe } from '../../Core/Pipe/splice-descreption.pipe';
 import { TooltipModule } from 'primeng/tooltip';
+import { GetoneTopicService } from '../../Service/getone-topic.service';
+import { IResponseOf } from '../../../../Core/Shared/Interface/irespose';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-view-topicand-stage',
   standalone: true,
-  imports: [RouterModule, TopPopComponent , SpliceDescreptionPipe ,TooltipModule],
+  imports: [RouterModule, TopPopComponent ,CommonModule, SpliceDescreptionPipe , DatePipe,TooltipModule],
   templateUrl: './view-topicand-stage.component.html',
   styleUrls:[ './view-topicand-stage.component.scss']
 })
 export class ViewTopicandStageComponent {
 
 
-  topicId: string | null = null; // To store the 'id' param
+  topicId: number = 0; 
   text:string ='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sunt atque, dolor sit quasi quos assumenda ratione ea exercitationem illo vitae cumque accusantium nisi omnis sequi dignissimos debitis id odit cum porro doloribus deserunt dolore? Blanditiis cumque inventore eius, dolor error fugit vero asperiores voluptas assumenda eveniet optio. Assumenda eos, sed, nesciunt est nobis quam nihil velit, maxime id a tenetur alias vitae voluptatum ipsam amet quia fugit reiciendis magni blanditiis quas molestiae. Odio quisquam optio officiis nostrum dolorum at quod saepe temporibus, repellendus rerum doloremque laboriosam illo suscipit molestias! Porro, id. Repellat explicabo minus ullam ad ipsam ipsa? Libero?'
-
+  TopicResult: IResponseOf<TopicResult> = {} as IResponseOf<TopicResult>;
+  
+  private _getoneTopicService = inject(GetoneTopicService)
   constructor(private _ActivatedRoute: ActivatedRoute, private _Router: Router) {}
   closeViewTopic(){
     this._Router.navigate([{ outlets: { dialog: null } }]);
@@ -29,6 +35,17 @@ export class ViewTopicandStageComponent {
       console.log('Topic ID:', this.topicId); 
     });
     console.log('Topic ID:', this.topicId); 
+    this._getoneTopicService.getTopicById(this.topicId).subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.TopicResult.result = response.result
+          console.log(response)
+        }
+      },
+      error: (error) => {
+        console.error('Error:', error);
+          }
+    })  
 
   }
 }
--- src/app/Pages/Topics/Service/getone-topic.service.ts
+++ src/app/Pages/Topics/Service/getone-topic.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../Environments/environment';
+import { Observable } from 'rxjs';
+import { IResponseOf } from '../../../Core/Shared/Interface/irespose';
+import { TopicResult } from '../Core/Interface/itopic';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetoneTopicService {
 
-  constructor() { }
+  private getTopic: string;
+
+  constructor(private _HttpClient :HttpClient) {
+    this.getTopic = `${environment.baseUrl}${environment.pickup}topic/get`;
+   }
+
+    getTopicById(topic: number): Observable<IResponseOf<TopicResult>> {
+      return this._HttpClient.get<IResponseOf<TopicResult>>(`${this.getTopic}?id=${topic}`  )
+    }
 }
