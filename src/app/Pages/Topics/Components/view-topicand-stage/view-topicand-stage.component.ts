3ae318dc7524bb02f08d1ca8fddae8310b58f6c9
mostafa hamed <Mostafa7ahmed>
2025-03-09 23:12:39 +0200

    Enhance ViewTopicandStage component: add TextHeader component, improve topic fetching logic, and update styling for better UI consistency

--- src/app/Pages/Topics/Components/view-topicand-stage/view-topicand-stage.component.ts
+++ src/app/Pages/Topics/Components/view-topicand-stage/view-topicand-stage.component.ts
@@ -8,11 +8,12 @@ import { TooltipModule } from 'primeng/tooltip';
 import { GetoneTopicService } from '../../Service/getone-topic.service';
 import { IResponseOf } from '../../../../Core/Shared/Interface/irespose';
 import { CommonModule, DatePipe } from '@angular/common';
+import { TextHeaderComponent } from "../../../Courses/Components/text-header/text-header.component";
 
 @Component({
   selector: 'app-view-topicand-stage',
   standalone: true,
-  imports: [RouterModule, TopPopComponent ,CommonModule, SpliceDescreptionPipe , DatePipe,TooltipModule],
+  imports: [RouterModule, TopPopComponent, CommonModule, SpliceDescreptionPipe, DatePipe, TooltipModule, TextHeaderComponent],
   templateUrl: './view-topicand-stage.component.html',
   styleUrls:[ './view-topicand-stage.component.scss']
 })
@@ -20,8 +21,8 @@ export class ViewTopicandStageComponent {
 
 
   topicId: number = 0; 
-  text:string ='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sunt atque, dolor sit quasi quos assumenda ratione ea exercitationem illo vitae cumque accusantium nisi omnis sequi dignissimos debitis id odit cum porro doloribus deserunt dolore? Blanditiis cumque inventore eius, dolor error fugit vero asperiores voluptas assumenda eveniet optio. Assumenda eos, sed, nesciunt est nobis quam nihil velit, maxime id a tenetur alias vitae voluptatum ipsam amet quia fugit reiciendis magni blanditiis quas molestiae. Odio quisquam optio officiis nostrum dolorum at quod saepe temporibus, repellendus rerum doloremque laboriosam illo suscipit molestias! Porro, id. Repellat explicabo minus ullam ad ipsam ipsa? Libero?'
   TopicResult: IResponseOf<TopicResult> = {} as IResponseOf<TopicResult>;
+  bgColor:string ='';
   
   private _getoneTopicService = inject(GetoneTopicService)
   constructor(private _ActivatedRoute: ActivatedRoute, private _Router: Router) {}
@@ -29,23 +30,42 @@ export class ViewTopicandStageComponent {
     this._Router.navigate([{ outlets: { dialog: null } }]);
 
   }
-  ngOnInit(): void {
-    this._ActivatedRoute.params.subscribe(params => {
-      this.topicId = params['id']; 
-      console.log('Topic ID:', this.topicId); 
-    });
-    console.log('Topic ID:', this.topicId); 
-    this._getoneTopicService.getTopicById(this.topicId).subscribe({
+  getTopicById(topicId:number){
+    this._getoneTopicService.getTopicById(topicId).subscribe({
       next: (response) => {
         if (response.success) {
           this.TopicResult.result = response.result
-          console.log(response)
+          this.bgColor = this.convertHexToRgba(this.TopicResult.result.color, 0.4);
+          console.log(this.bgColor)
         }
       },
       error: (error) => {
         console.error('Error:', error);
           }
     })  
+  }
+  
+  convertHexToRgba(hex: string, opacity: number = 1): string {
+    hex = hex.replace('#', '');
+
+    if (hex.length !== 6) {
+      console.error('Invalid HEX color:', hex);
+      return '#3e97ff1a';
+    }
+
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+  }
+  ngOnInit(): void {
+    this._ActivatedRoute.params.subscribe(params => {
+      this.topicId = params['id']; 
+      this.getTopicById(this.topicId)
+    });
+
+
 
   }
 }
