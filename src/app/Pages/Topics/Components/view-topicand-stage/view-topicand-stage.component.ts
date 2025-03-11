f8a22f04373bc1bc9d952643a307e5c4d2b75022
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2025-03-11 02:27:22 +0200

    fix default topic from view

--- src/app/Pages/Topics/Components/view-topicand-stage/view-topicand-stage.component.ts
+++ src/app/Pages/Topics/Components/view-topicand-stage/view-topicand-stage.component.ts
@@ -28,6 +28,7 @@ export class ViewTopicandStageComponent {
   bgColor: string = '';
   showEdit: boolean = false;
   selectedStaged: number | null = null;
+  isDefaultTopic: boolean = false;
 
   private _getoneTopicService = inject(GetoneTopicService);
   private _convertColorService = inject(ConvertColorService);
@@ -64,6 +65,7 @@ export class ViewTopicandStageComponent {
       next: (response) => {
         if (response.success) {
           this.TopicResult.result = response.result;
+          this.isDefaultTopic = response.result.default;
           this.bgColor = this._convertColorService.convertHexToRgba(this.TopicResult.result.color, 0.4);
         }
       },
@@ -76,6 +78,7 @@ export class ViewTopicandStageComponent {
     this._SetDefaultTopicService.setDefaultTopic(topicId).subscribe({
       next: (res) => {
         if (res.success && res.result) {
+          this.isDefaultTopic = true;
           const { newDefaultTopicId, oldDefaultTopicId } = res.result;
           if (this.TopicResult?.result && Array.isArray(this.TopicResult.result)) {
             let newDefaultStage = this.TopicResult.result.find(topic => topic.id === newDefaultTopicId);
@@ -92,30 +95,30 @@ export class ViewTopicandStageComponent {
         console.error('Error:', error);
       }
     });
-}
+  }
 
-setDefaultStage(id: number, topicId: number): void {
-  this._SetDefaultStageService.setDefaultStage(topicId, id).subscribe({
-    next: (res) => {
-      if (res.success && res.result) {
-        const { newDefaultTopicId, oldDefaultTopicId } = res.result;
-        if (this.TopicResult?.result?.stages && Array.isArray(this.TopicResult.result.stages)) {
-          let newDefaultStage = this.TopicResult.result.stages.find(stage => stage.id === newDefaultTopicId);
-          if (newDefaultStage) newDefaultStage.default = true;
-          let oldDefaultStage = this.TopicResult.result.stages.find(stage => stage.id === oldDefaultTopicId);
-          if (oldDefaultStage) oldDefaultStage.default = false;
+  setDefaultStage(id: number, topicId: number): void {
+    this._SetDefaultStageService.setDefaultStage(topicId, id).subscribe({
+      next: (res) => {
+        if (res.success && res.result) {
+          const { newDefaultTopicId, oldDefaultTopicId } = res.result;
+          if (this.TopicResult?.result?.stages && Array.isArray(this.TopicResult.result.stages)) {
+            let newDefaultStage = this.TopicResult.result.stages.find(stage => stage.id === newDefaultTopicId);
+            if (newDefaultStage) newDefaultStage.default = true;
+            let oldDefaultStage = this.TopicResult.result.stages.find(stage => stage.id === oldDefaultTopicId);
+            if (oldDefaultStage) oldDefaultStage.default = false;
+          }
+          this.toggleShowStage(null)
+        } else {
+          console.error('Failed to set default topic:', res);
         }
-        this.toggleShowStage(null)
-      } else {
-        console.error('Failed to set default topic:', res);
+      },
+      error: (error) => {
+        console.error('Error:', error);
       }
-    },
-    error: (error) => {
-      console.error('Error:', error);
-    }
-  });
-}
-  
+    });
+  }
+
   openPopupAddStage() {
 
     this._Router.navigate([
