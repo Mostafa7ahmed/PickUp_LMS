6f4985564fc4b012f09bd8997bab195c18432720
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2025-03-20 01:35:29 +0200

    remove un used lines

--- src/app/Environments/environment.ts
+++ src/app/Environments/environment.ts
@@ -3,10 +3,4 @@ export const environment = {
     baseUrl: 'https://pickup.runasp.net',
     pickup:'/pickup-lms/api/v1/',
     baseUrlFiles:'https://vgfdprgqzkwvoxsnfdlr.supabase.co/storage/v1/object/public/pickup/'
-    
-
-
-    
-
-
   };
