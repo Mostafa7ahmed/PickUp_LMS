ab42ffbf6a2973b36e944d05efc275f19fa725bd
mostafa hamed <Mostafa7ahmed>
2025-03-20 20:25:33 +0200

    fix: update splicText pipe to accept a dynamic character limit and adjust usage in table-courses component

--- src/app/Pages/Courses/Core/Pipes/splic-text.pipe.ts
+++ src/app/Pages/Courses/Core/Pipes/splic-text.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true
 })
 export class SplicTextPipe implements PipeTransform {
-  transform(value: string): string {
-    if (!value) return '';
-    return value.length > 40 ? value.substring(0, 40) + '...' : value;
-  }
+    transform(value: string, limit: number = 40): string {
+  if (!value) return '';
+  return value.length > limit ? value.substring(0, limit) + '...' : value;
+}
 }
