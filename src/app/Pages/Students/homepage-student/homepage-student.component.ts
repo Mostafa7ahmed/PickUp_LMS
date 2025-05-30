a17c560e1d14b439af2c7f56c7878154fa3882bb
mostafa hamed <Mostafa7ahmed>
2025-05-30 17:10:42 +0300

    feat: add course card component with HTML, SCSS, and TypeScript implementation
    - Created course card component to display a list of courses with details such as image, instructor, duration, lessons, rating, and pricing.
    - Implemented responsive design using SCSS for styling the course cards.
    - Added unit tests for the course card component to ensure functionality.

--- src/app/Pages/Students/homepage-student/homepage-student.component.ts
+++ src/app/Pages/Students/homepage-student/homepage-student.component.ts
@@ -3,11 +3,12 @@ import { MyCoursesComponent } from "./components/my-courses/my-courses.component
 import { AssignmentsComponent } from "./components/assignments/assignments.component";
 import { TodoComponent } from "./components/todo/todo.component";
 import { ResourcesComponent } from "./components/resources/resources.component";
+import { CourseCardComponent } from "./components/course-card/course-card.component";
 
 @Component({
   selector: 'app-homepage-student',
   standalone: true,
-  imports: [MyCoursesComponent, AssignmentsComponent, TodoComponent, ResourcesComponent],
+  imports: [MyCoursesComponent, AssignmentsComponent, TodoComponent, ResourcesComponent, CourseCardComponent],
   templateUrl: './homepage-student.component.html',
   styleUrl: './homepage-student.component.scss'
 })
