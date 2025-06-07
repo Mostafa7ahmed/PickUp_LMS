84a86fa1b7df756b3160896a6ee72b56920f4c28
mostafa hamed <Mostafa7ahmed>
2025-06-07 17:52:05 +0300

    feat(view-lesson): enhance lesson component with tabs and dynamic course ID binding

--- src/app/Pages/lesson/Components/view-lesson/view-lesson.component.ts
+++ src/app/Pages/lesson/Components/view-lesson/view-lesson.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-view-lesson',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule , TabsModule],
   templateUrl: './view-lesson.component.html',
   styleUrl: './view-lesson.component.scss'
 })
 export class ViewLessonComponent {
   constructor(private location: Location) {}
+  value: number = 0;
 
   goBackToCourse() {
     this.location.back();
