279534e1d4c6f5638fa32b8e4e5f70ce6a5d15fd
mostafa hamed <Mostafa7ahmed>
2025-06-29 13:41:04 +0300

    Rating

--- src/app/Pages/Students/discover-course-details/reating-cards/reating-cards.component.ts
+++ src/app/Pages/Students/discover-course-details/reating-cards/reating-cards.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ReviewsByDate } from '../../../rating/Core/interface/irating';
 import { Rating } from '../Core/Interface/ires-course-details-discover';
+import { environment } from '../../../../Environments/environment';
 
 @Component({
   selector: 'app-reating-cards',
@@ -12,6 +13,7 @@ import { Rating } from '../Core/Interface/ires-course-details-discover';
 })
 export class ReatingCardsComponent {
    @Input()  reviewsByDates: Rating[] = [];
+   baseUrl:string = environment.baseUrlFiles
 
   getStarsArray(stars: number): number[] {
     return Array(Math.floor(stars)).fill(0);
